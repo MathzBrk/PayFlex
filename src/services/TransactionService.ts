6e589bcb338084818b783ef6caed2763ebf589de
Matheus Borges <matheusvbborges@hotmail.com>
2025-02-02 02:13:06 -0300

    feat(transaction): adicionar axios para consumir o mock e criar método authorizeTransaction

--- src/services/TransactionService.ts
+++ src/services/TransactionService.ts
@@ -4,6 +4,7 @@ import { TransactionRepository } from "../repositories/TransactionRepository";
 import { TransactionCreateValidator } from "../domain/transaction/validator/transactionCreateValidator";
 import {UserRepository} from "../repositories/UserRepository";
 import * as console from "node:console";
+import axios from "axios";
 
 export class TransactionService {
 
@@ -11,6 +12,7 @@ export class TransactionService {
     private transactionRepository: TransactionRepository;
     private transactionValidator: TransactionCreateValidator;
     private userRepository: UserRepository;
+    private AUTHORIZATION_URL = "https://util.devi.tools/api/v2/authorize";
 
     constructor() {
         this.transactionRepository = new TransactionRepository();
@@ -23,6 +25,11 @@ export class TransactionService {
         try{
             await this.transactionValidator.validate(transactionDto);
 
+            const isAuthorized = await this.authorizeTransaction();
+            if(!isAuthorized){
+                throw new Error("Unauthorized transaction");
+            }
+
             console.log("Transaction created successfully.");
 
             return this.transactionRepository.createTransaction(transactionDto);
@@ -31,6 +38,23 @@ export class TransactionService {
         } catch(error){
             console.log("Error during creating transaction ", error);
             throw error;
+        }
+    }
+
+    private async authorizeTransaction(): Promise<boolean> {
+        try {
+            const response = await axios.get(this.AUTHORIZATION_URL);
+
+            if(response.data.hasOwnProperty('data') && response.data.data && typeof response.data.data.authorization === "boolean"){
+                console.log("Transaction authorized", response.data.data.authorization);
+                return response.data.data.authorization;
+            } else{
+                console.log("Error during authorize transaction", response);
+                return false;
+            }
+        } catch (error) {
+            console.error("authorization error ", error);
+            return false;
         }
     }
 
